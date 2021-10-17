183cf76443974505a7bbeb6c83bf0d0b7ea3122a
mokumoku-chan <maho.kumo1997@gmail.com>
2021-10-17 09:39:05 +0000

    [Add] マイページ編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+
   get 'homes/about'
 
   resources :users, only: [:index, :show, :edit]
