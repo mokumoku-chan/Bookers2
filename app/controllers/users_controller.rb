e711c1263cf7279f81e4e8b6b5abb89cb341c885
mokumoku-chan <maho.kumo1997@gmail.com>
2021-10-17 12:01:04 +0000

    [Add] Book一覧、詳細表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    @book = Book.find(user.id)
   end
 
   def edit
     @user = User.find(params[:id])
+
   end
 
   def update
